c90e1d60db5941836fcd0eadfa426bf46a59a6c4
Piotr Adamczyk <xeno@xeno.yt>
2020-02-29 19:24:11 +0100

    initial commit (hey it works!)

---
+++ campaignAbi.js
@@ -0,0 +1,370 @@
+module.exports = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "daiAddress",
+        type: "address"
+      },
+      {
+        internalType: "address",
+        name: "cdaiAddress",
+        type: "address"
+      },
+      {
+        internalType: "address",
+        name: "raiser",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "mediumPostUUID",
+        type: "bytes32"
+      },
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "bodyHash",
+        type: "bytes32"
+      }
+    ],
+    name: "Announcement",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "raisedDai",
+        type: "uint256"
+      }
+    ],
+    name: "Closed",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "RaiserWithdraw",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "investor",
+        type: "address"
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "SupporterDeposit",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "investor",
+        type: "address"
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "SupporterWithdraw",
+    type: "event"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    name: "_supportersCollateralBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    name: "_supportersSubmissions",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "mediumPostUUID",
+        type: "bytes32"
+      },
+      {
+        internalType: "bytes32",
+        name: "bodyHash",
+        type: "bytes32"
+      }
+    ],
+    name: "announce",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "close",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getCDaiAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getCollateralBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getDaiAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getDaiRaised",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getRaiser",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getSubmission",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "supporter",
+        type: "address"
+      }
+    ],
+    name: "getSupporterCollateralBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "supporter",
+        type: "address"
+      }
+    ],
+    name: "getSupporterSubmission",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "isClosed",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "withdrawnSupportingDaiAmount",
+        type: "uint256"
+      }
+    ],
+    name: "stopSupporting",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "support",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "withdraw",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  }
+];
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+const ethers = require("ethers");
+const sqlite3 = require("sqlite3").verbose();
+const campaignAbi = require("./campaignAbi");
+const bodyParser = require("body-parser");
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rinkeby.infura.io/v3/d456fa9795db4641a0989d8016b3414e"
+);
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = new sqlite3.Database("./db.sqlite3");
+
+db.serialize(function() {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS campaigns (`address` TEXT NOT NULL PRIMARY KEY, data TEXT NOT NULL)"
+  );
+});
+
+app.get("/campaign/:address", (req, res) => {
+  const campaignAddress = req.params.address;
+  db.serialize(() => {
+    db.get(
+      "SELECT data FROM campaigns WHERE address like ?",
+      [campaignAddress],
+      (error, data) => {
+        if (error) {
+          return res.json({ success: false, error: error.message });
+        }
+        if (!data) {
+          return res.json({ success: false, error: "no data" });
+        }
+        res.json({ success: true, data: JSON.parse(data.data) });
+      }
+    );
+  });
+});
+
+app.post("/campaign/:address", async (req, res) => {
+  const campaignAddress = req.params.address;
+  const campaignContract = new ethers.Contract(
+    campaignAddress,
+    campaignAbi,
+    provider
+  );
+  let raiser;
+  try {
+    raiser = await campaignContract.getRaiser();
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message
+    });
+    return;
+  }
+  const address = ethers.utils.verifyMessage(
+    JSON.stringify(req.body.data),
+    req.body.signature
+  );
+  if (address !== raiser) {
+    res.json({
+      success: false,
+      message: "recovered address mismatch",
+      recoveredAddress: address,
+      raiserAddress: raiser
+    });
+    return;
+  }
+  db.serialize(() => {
+    db.get(
+      "INSERT OR REPLACE INTO campaigns(address, data) VALUES(?, ?)",
+      [campaignAddress, JSON.stringify(req.body.data)],
+      (error, data) => {
+        if (error) {
+          return res.json({ success: false, error: error.message });
+        }
+        res.json({ success: true, ...data });
+      }
+    );
+  });
+});
+
+app.listen(8080);
